ad1708f8227cc121c02be6da1156288c846e8811
srikar0905 <83820148+srikar0905@users.noreply.github.com>
2021-08-04 18:59:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Boot from "./Boot";
+
+function App(){
+  return <div>
+  <Header />
+  <Boot />
+  <Footer />
+  </div>;
+}
+
+export default App;
---
+++ Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot1() {
+  return (
+    <div>
+      <h1>Event 1</h1>
+      <p>Description of the event</p>
+    </div>
+  );
+}
+
+export default Boot1;
